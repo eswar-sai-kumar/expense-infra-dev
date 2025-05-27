564027802ef663dfa95c38892381bab4f828c4f7
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-05-27 14:47:37 +0530

    update

--- 05-vpn/data.tf
+++ 05-vpn/data.tf
@@ -1,19 +1,6 @@
-data "aws_ssm_parameter" "backend_sg_id" {
-  name = "/${var.project_name}/${var.environment}/backend_sg_id"
+data "aws_ssm_parameter" "vpn_sg_id" {
+  name = "/${var.project_name}/${var.environment}/vpn_sg_id"
 }
-
-data "aws_ssm_parameter" "frontend_sg_id" {
-  name = "/${var.project_name}/${var.environment}/frontend_sg_id"
-}
-
-data "aws_ssm_parameter" "ansible_sg_id" {
-  name = "/${var.project_name}/${var.environment}/ansible_sg_id"
-}
-
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
-}
-
 data "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 }
@@ -22,11 +9,11 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 data "aws_ami" "ami_info" {
 
     most_recent = true
-    owners = ["973714476881"]
+    owners = ["679593333241"]
 
     filter {
         name   = "name"
-        values = ["RHEL-9-DevOps-Practice"]
+        values = ["OpenVPN Access Server Community Image-fe8020db-*"]
     }
 
     filter {
--- 05-vpn/main.tf
+++ 05-vpn/main.tf
@@ -1,90 +1,24 @@
-module "backend" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "${var.project_name}-${var.environment}-backend"
-
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg_id.value]
-  # convert StringList to list and get first element
-  subnet_id = local.private_subnet_id
-  ami = data.aws_ami.ami_info.id
-  tags = merge(
-    var.common_tags,
-    {
-        Name = "${var.project_name}-${var.environment}-backend"
-    }
-  )
+resource "aws_key_pair" "vpn" {
+  key_name   = "vpn"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJU30dGg3ihXj7HSH9QG6Juz0MiAgbxpnZjy14Ie/xK0vQyPiBUOy48jUxbfkt1TcHN+fRE5wfniYv9vkT0Q9VPrpMXJeo3mJWBNqp41LUfPkbvxJRjhHmRg+/rlbhD8T1iSwmqzuE7TkuzbyYmsrLwFYgzcQqPeja1G8qUWJcBXJQdmdy6dDZ3u+ZAuLMRE6Rm5POCjSvCG67EXAdIWClIu27XGcoFap6a/+zYXSXfhVcu6FzRRZerW7IgIZ9w2vmNPMjrtdFSQsXe3HgejnwwdY+icLC6dy3TGNTWymFbhAhZDD5WfRJid6vnUZO5l8TDUvuuSQWfs1tPX0hMnjc0uMWWbr/1O6WwqQbYZR6ChhGrc8VzPA2CIwy//K2HmG7s0WnnR+6LGgcIp5h7cDB4NHWf8WOFf2FGwWLVrlJwynT3wq3uWtCY1O0LiFLLFHmn54YvBCsvUzGExxDgm6Ko6ec2rqEYEQhuGF0HVyfbD5e1boJ1bO+TE71qmX3NlhyN7UpKA5awQzJB/mjuMrcAp0h8ZMeTMt4jUwsl5VB32vcOFoxkTpvD5EK1X0f6gC5HROBSPvNoVlfonwAeHx31/c8mPEuhpGljWA8t6a/3csOF2ANNQg9TBd2zc+ovlEpZUy6pwdGruLRywhUEglbaqkktpvNaEyPf9WwUHU/UQ== 91995@saikumarsPC"
+  # ~ means windows home directory
 }
 
-module "frontend" {
+module "vpn" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "${var.project_name}-${var.environment}-frontend"
+  key_name = aws_key_pair.vpn.key_name
+  name = "${var.project_name}-${var.environment}-vpn"
 
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [data.aws_ssm_parameter.frontend_sg_id.value]
+  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
   # convert StringList to list and get first element
   subnet_id = local.public_subnet_id
   ami = data.aws_ami.ami_info.id
+  
   tags = merge(
     var.common_tags,
     {
-        Name = "${var.project_name}-${var.environment}-frontend"
+        Name = "${var.project_name}-${var.environment}-vpn"
     }
   )
-}
-
-module "ansible" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "${var.project_name}-${var.environment}-ansible"
-
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [data.aws_ssm_parameter.ansible_sg_id.value]
-  # convert StringList to list and get first element
-  subnet_id = local.public_subnet_id
-  ami = data.aws_ami.ami_info.id
-  user_data = file("expense.sh")
-  tags = merge(
-    var.common_tags,
-    {
-        Name = "${var.project_name}-${var.environment}-ansible"
-    }
-  )
-  depends_on = [ module.backend,module.frontend ]
-}
-
-module "records" {
-  source  = "terraform-aws-modules/route53/aws//modules/records"
-  version = "~> 2.0"
-
-  zone_name = var.zone_name
-
-  records = [
-    {
-      name    = "backend"
-      type    = "A"
-      ttl     = 1
-      records = [
-        module.backend.private_ip
-      ]
-    },
-    {
-      name    = "frontend"
-      type    = "A"
-      ttl     = 1
-      records = [
-        module.frontend.private_ip
-      ]
-    },
-    {
-      name    = "" # eswarsaikumar.site
-      type    = "A"
-      ttl     = 1
-      records = [
-        module.frontend.public_ip
-      ]
-    },
-  ]
-
 }
