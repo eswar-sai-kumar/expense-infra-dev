5682b972b60e440f4b3d87e53e5d997bc36c9ac5
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-06-01 08:47:39 +0530

    update

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -1,6 +1,16 @@
+
+
 #!/bin/bash
+set -euxo pipefail # Add this line!
+
 component=$1
 environment=$2
+
+echo "Attempting to install Ansible..."
 dnf install ansible -y
+
+echo "Attempting to install Python packages..."
 pip3.9 install botocore boto3
+
+echo "Attempting to run ansible-pull..."
 ansible-pull -i localhost, -U https://github.com/eswar-sai-kumar/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
